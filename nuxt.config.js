b964670b5520136329ad0d5922480b3442cd4b37
conoha <xxx>
2021-04-10 00:10:23 +0900

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+  components: true,
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'ポートフォリオ | マキタ ナオキ',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'マキタナオキのポートフォリオサイトです。webページなどの個人製作を行っています。' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'マキタ ナオキ' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:title', property: 'og:title', content: 'ポートフォリオ | マキタ ナオキ' },
+      { hid: 'og:description', property: 'og:description', content: 'マキタナオキのポートフォリオサイトです。webページなどの個人製作を行っています。' },
+      { hid: 'og:url', property: 'og:url', content: 'https://portfolio.klavier.page' },
+      { hid: 'og:image', property: 'og:image', content: '/naoki_makita_og.png' }
+    ],
+    link: [
+      { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/gridlex/2.3.1/gridlex.min.css" },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'canonical', href: 'https://portfolio.klavier.page' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    '@nuxtjs/google-fonts',
+    '@nuxt/image'
+  ],
+
+  googleFonts: {
+    families: {
+      'Noto+Sans+JP': true,
+      'Kelly+Slab': true,
+    },
+    display: 'swap'
+  },
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    '@nuxtjs/dotenv',
+    '@nuxtjs/axios',
+    '@nuxtjs/google-gtag'
+  ],
+
+  'google-gtag': {
+    id: '-',
+  },
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {
+  },
+  sitemap: {
+    path: '/sitemap.xml',
+    hostname: 'https://portfolio.klavier.page/'
+  },
+  router: {
+    extendRoutes (routes, resolve) {
+      routes.push({
+        name: '404error',
+        path: '*',
+        component: resolve('~/pages/index.vue')
+      })
+    }
+ },
+}
---
+++ pages/index.vue
@@ -0,0 +1,525 @@
+<template>
+  <div class="grid col-12">
+    <div class="logo bg-birthday-color-sub">
+      <div class="logo-text">
+        <h1>NAOKI MAKITA</h1>
+        <p class="h1-p">In-house System Engineer</p>
+      </div>
+    </div>
+    <div class="col-12 works">
+      <div class="grid-center waku">
+        <div class="col-12">
+          <h2>In-house Works</h2>
+        </div>
+        <div v-for="item in item_json" v-if="item['id'] < 3" class="grid col-4_sm-8 item" @click="openModal(item['id'])">
+          <nuxt-img class="item-img" :width="img_w_item" :height="img_h_item" :src="item['img']" style="object-fit: cover;width: 100%" loading="lazy" alt="header-img" />
+          {{item["title"]}}
+        </div>
+      </div>
+    </div>
+    <div class="col-12 works bg-birthday-color-sub">
+      <div class="grid-center waku">
+        <div class="col-12">
+          <h2>Private Works</h2>
+        </div>
+        <div v-for="item in item_json" v-if="item['id'] > 2" class="grid col-4_sm-8 item" @click="openModal(item['id'])">
+          <nuxt-img class="item-img" :width="img_w_item" :height="img_h_item" :src="item['img']" style="object-fit: cover;width: 100%" loading="lazy" alt="header-img" />
+          {{item["title"]}}
+        </div>
+      </div>
+    </div>
+    <div class="col-12 works">
+      <div class="grid-center waku">
+        <div class="col-12">
+          <h2>Skills</h2>
+        </div>
+        <div class="col-4_sm-6 item">
+          <div class="circle anime2 rails"><div class="circle-inner"><p style="margin: 0;font-size: .8rem;letter-spacing: 0;">Ruby on Rails</p></div></div>
+        </div>
+        <div class="col-4_sm-6 item">
+          <div class="circle anime2 nuxt"><div class="circle-inner">Nuxt.js</div></div>
+        </div>
+        <div class="col-4_sm-12 item">
+          <div class="circle anime2 html"><div class="circle-inner">HTML</div></div>
+        </div>
+        <div class="grid-center col-10_sm-12">
+          <div class="col-5_sm-6 item">
+            <div class="circle anime3 filemaker"><div class="circle-inner">FileMaker</div></div>
+          </div>
+          <div class="col-5_sm-6 item">
+            <div class="circle anime2 gas"><div class="circle-inner">GAS</div></div>
+          </div>
+        </div>
+        <div class="col-4_sm-12 item">
+          <div class="circle anime1 sql"><div class="circle-inner">SQL</div></div>
+        </div>
+        <div class="col-4_sm-6 item">
+          <div class="circle anime1 gcp"><div class="circle-inner">GCP</div></div>
+        </div>
+        <div class="col-4_sm-6 item">
+          <div class="circle anime1 live"><div class="circle-inner">ライブ配信</div></div>
+        </div>
+
+      </div>
+    </div>
+    <div class="col-12 works bg-birthday-color-sub">
+      <div class="grid waku">
+        <div class="col-12">
+          <h2>About</h2>
+        </div>
+        <div class="about" style="margin: auto">
+          <p style="display:inline-block;vertical-align:top;margin: 0">
+            <nuxt-img class="" :width="112" src="/naoki_makita.jpg" style="object-fit: cover;border-radius: 1rem;" loading="lazy" alt="header-img" />
+          </p>
+          <br class="display-none-pc">
+          <div class="about-div" style="display:inline-block;vertical-align:top">
+            <h3 class="col-12">
+              マキタ　ナオキ
+            </h3>
+            <div class="col-12">
+              <p>webページなどの個人製作を行っています。<br>
+              <a href="https://twitter.com/miraxial">ご依頼はDMまで。</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <transition name="modal" appear v-if="modal">
+      <div class="modal modal-overlay" @click="closeModal">
+        <div class="modal-window" style="max-height: 90vh">
+          <div class="modal-header" style="position: relative">
+            {{ title }}
+            <button @click="closeModal" style="position: absolute;right: .6rem;top: .6rem;background: none;border: none;color: #555">×</button>
+          </div>
+          <div class="modal-content" style="overflow-y: auto;max-height: 80vh;">
+            <div class="">
+              <div class="">
+                <div style="position: relative;text-align: center">
+                  <nuxt-img class="item-img" :width="img_w_modal" :height="img_h_modal" :src="img" style="object-fit: cover;max-width: 100%" loading="lazy" alt="header-img" />
+                </div>
+              </div>
+              <div class="" v-html="text">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        img_w_item: "1024px",
+        img_h_item: "1024px",
+        img_w_modal: "1024px",
+        img_h_modal: "1024px",
+        modal: false,
+        img: undefined,
+        vw: undefined,
+        title: "",
+        item_json: [
+          {id: 0, title: "保守・運用", img: "/i1.jpg",
+          text: "<h3>◆&nbsp仕事内容</h3><p>社員30名、アルバイト30名程度の事業会社で社内の保守・運用をしてきました。うちシステム部５名、キャリア４年。</p><h3>◆&nbspできること（システム）</h3><p>リモート環境構築／システム機器購入／社内サーバーの管理／Gmailなどの外部アプリやPC、社内システムの使い方レクチャー・不具合対処</p><h3>◆&nbsp楽しみ</h3><p>「この作業、もっと楽にできないかな？」の解決を楽しみにしています。<br>その為には、社内ほか事業部の情報インプットが大事です。部分最適にならないように気を付けつつ、素早くシステム改善ができることが社内SEの強みだと考えています。</p>"
+          },
+          {id: 1, title: "カスタマー対応", img: "/i2.jpg",
+          text: "<h3>◆&nbsp仕事内容</h3><p>電話応対・メール応対・来客応対を週10時間程度行っています。<br>主にWebページの使い方や不明点の説明、決済などによるシステムエラーの状況伺いなどをしますが、事業にまつわるものも対応可能な範囲のものは回答します。<br>問合せの原因がシステム不具合だった場合は、お客様への対応と共に、Webシステム(CakePHP , Ruby on Rails , Vue.js)の改修やMySQLなどのデータ操作、再発防止の対策をします。</p><h3>◆&nbsp楽しみ</h3><p>お客様の作業時間を減らすことを楽しみにしています。<br>サポートを受ける場合、大体、お客様は何かに困っています。そのお客様に対して、「その情報はここから閲覧できますよ」などのピンポイントな解決方法だけでなく、「ここに困るということは、こういう情報も足りてないのでは？」など＋αでお手伝いし、お客様の無駄な時間を減らすことに試行錯誤しています。</p>"
+          },
+          {id: 2, title: "社内システム改修", img: "/i3.jpg",
+          text: "<h3>◆&nbsp仕事内容</h3><p>FileMakerを用いた業務改善を行っています。ClarisEngage2020登壇<p>"
+          },
+          {id: 3, title: "イベントスペース", img: "/p1.png",
+          text: "<p>ROLE：Design／Coding／Writing<br>URL：<a href='https://sv-rikugien.klavier.page/'>https://sv-rikugien.klavier.page/</p>"
+          },
+          {id: 4, title: "音楽教室", img: "/p2.png",
+          text: "<p>ROLE：Coding<br>URL：<a href='https://cjmtokyo.com/'>https://cjmtokyo.com/</p>"
+          },
+          {id: 5, title: "宿泊所", img: "/p3.png",
+          text: "<p>ROLE：Design／Coding／Writing<br>URL：<a href='https://launion.page/'>https://launion.page/</p>"
+          },
+        ]
+      }
+    },
+    methods: {
+      openModal(number) {
+        this.modal = true;
+        this.title = this.item_json[number]["title"];
+        this.img = this.item_json[number]["img"];
+        this.text = this.item_json[number]["text"]
+      },
+      closeModal() {
+        this.modal = false
+      }
+    },
+    mounted() {
+      this.vw = window.innerWidth;
+      if(this.vw > 768){
+        this.img_w_item = 240;
+        this.img_w_modal = 360;
+      }
+      else{
+        this.img_w_item = parseInt( this.vw * 2 / 3 - 20)
+        this.img_w_modal = this.img_w_item
+      }
+      this.img_h_item = parseInt(this.img_w_item * 2 / 3)
+      this.img_h_modal = "auto"
+      this.img_w_header_img = this.vw;
+    },
+  }
+</script>
+
+<style>
+.logo {
+  margin: auto;
+  width: 100%;
+  height: 100vh;
+  position: relative;
+}
+
+.logo-text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  margin:0;
+  padding:0;
+  width: 100vw;
+}
+
+.font-kelly {
+  font-family: 'Kelly Slab', cursive;
+}
+
+.bg-birthday-color {
+  background: #7d7b83;
+}
+
+.bg-birthday-color-sub {
+  background: #f0e3ff;
+}
+
+#page .item {
+  padding: 10px;
+  text-decoration: underline;
+  margin: 1rem 0;
+}
+
+.waku {
+  max-width: 780px;
+  margin: auto;
+}
+
+
+h1 {
+  font-family: 'Kelly Slab', cursive;
+  text-align: center;
+  margin: 0;
+  font-size: 2rem;
+  font-weight: 100;
+  line-height: 1;
+  letter-spacing: .2rem;
+  color: #484554;
+}
+
+.h1-p {
+  font-family: 'Kelly Slab', cursive;
+  color: #999;
+  letter-spacing: .3rem;
+  text-align: center;
+  margin: 0;
+}
+
+.works {
+  padding: 3vh 0 20vh 0;
+}
+
+.about {
+  text-align: center;
+}
+
+h2 {
+  font-family: 'Kelly Slab', cursive;
+  text-align: center;
+  letter-spacing: .5rem;
+}
+
+.item-img {
+  margin-bottom: .3rem;
+}
+
+.circle {
+    position: relative;
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    text-align: center;
+    overflow: hidden;
+    z-index: 1;
+    margin: auto;
+}
+
+.html {
+  background: #DD4B25;
+}
+
+.html::before , .html::after{
+  background: #E4A28D;
+}
+
+.rails {
+  background: #C60000;
+}
+
+.rails::before , .rails::after{
+  background: #FFE6D9;
+}
+
+.nuxt {
+  background: #00BF8A;
+}
+
+.nuxt::before , .nuxt::after{
+  background: #CCFBE5;
+}
+
+.filemaker {
+  background: #438BAA;
+}
+
+.filemaker::before , .filemaker::after{
+  background: #E0EEEB;
+}
+
+.gas {
+  background-image: radial-gradient(circle at -16.45% 25.82%, #E34133 0, #E34133 10%, #4081EC 20%, #E34133 30%, #F3B604 40%, #32A350 50%, #4081EC 60%, #E34133 70%, #F3B604 80%, #32A350 90%, #4081EC 100%);
+}
+
+.gas::before , .gas::after{
+  background-image: radial-gradient(circle at -16.45% 25.82%, #FFEECB 0, #D8F9DA 10%, #E7EFFF 20%, #FFE5DB 30%, #FFEECB 40%, #D8F9DA 50%, #E7EFFF 60%, #FFE5DB 70%, #FFEECB 80%, #D8F9DA 90%, #E7EFFF 100%);
+}
+
+.sql {
+  background: #F0951F;
+}
+
+.sql::before , .sql::after{
+  background: #FFEBCD;
+}
+
+.gcp {
+  background: #316BDF;
+}
+
+.gcp::before , .gcp::after{
+  background: #EBEEFF;
+}
+
+.live {
+  background: #FF0000;
+}
+
+.live::before , .live::after{
+  background: #FFE6D9;
+}
+
+
+
+.circle::before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    left: -60px;
+    width: 120px;
+    height: 120px;
+    transform-origin: right 60px;
+    z-index: 2;
+}
+
+.circle::after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0px;
+    left: 60px;
+    width: 120px;
+    height: 120px;
+    transform-origin: left 60px;
+    z-index: 3;
+}
+
+.anime1::before {
+  animation: rotate-circle-left-110 6s linear infinite;
+}
+.anime1::after {
+  animation: rotate-circle-right-110 6s linear infinite;
+}
+
+.anime2::before {
+  animation: rotate-circle-left-170 6s linear infinite;
+}
+.anime2::after {
+  animation: rotate-circle-right-170 6s linear infinite;
+}
+
+.anime3::before {
+  animation: rotate-circle-left-240 6s linear infinite;
+}
+.anime3::after {
+  animation: rotate-circle-right-240 6s linear infinite;
+}
+
+
+
+
+.circle .circle-inner {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    width: 100px;
+    height: 62px;
+    padding-top: 38px;
+    background: #fff;
+    border-radius: 50%;
+    z-index: 4;
+}
+
+.modal-overlay {
+  align-items: center;
+  background: rgba(0, 0, 0, .5);
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  left: 0;
+  padding: 5vh 0;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 30;
+}
+.modal-window {
+  background: #FFF;
+  border-radius: 4px;
+  overflow: hidden;
+  width: 90%;
+  margin: auto;
+}
+.modal-content {
+  padding: .5rem 1rem;
+}
+.modal-header {
+  background: #CEE8E4;
+  padding: 10px;
+  text-align: left;
+}
+.modal-enter-active , .modal-leave-active {
+  transition: opacity .4s;
+}
+.modal-enter-active .modal-window {
+  transition: opacity .4s, transform .4s;
+}
+.modal-leave-active .modal-window {
+  transition: opacity .4s, transform .4s;
+}
+.modal-leave-active {
+  transition: opacity .6s ease .4s;
+}
+.modal-enter , .modal-leave-to {
+  opacity: 0;
+}
+.modal-enter .modal-window {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+.modal-leave-to .modal-window {
+  opacity: 0;
+  transform: translateY(-20px);
+}
+
+.modal-content p {
+  padding-bottom: 1rem;
+}
+
+
+@keyframes rotate-circle-right-110 {
+    0%   {
+        transform: rotate(0deg);
+    }
+    30%  {
+        transform: rotate(110deg);
+    }
+    100% {
+        transform: rotate(110deg);
+    }
+}
+
+@keyframes rotate-circle-right-170 {
+    0%   {
+        transform: rotate(0deg);
+    }
+    30%  {
+        transform: rotate(170deg);
+    }
+    100% {
+        transform: rotate(170deg);
+    }
+}
+
+@keyframes rotate-circle-right-240 {
+    0%   {
+        transform: rotate(0deg);
+        background: #E0EEEB;
+    }
+    22.5%  {
+        transform: rotate(180deg);
+        background: #E0EEEB;
+    }
+    22.6% {
+        transform: rotate(0deg);
+        background: #438BAA;
+    }
+    100% {
+        transform: rotate(0deg);
+        background: #438BAA;
+    }
+}
+
+@keyframes rotate-circle-left-240 {
+    0%   { transform: rotate(0deg); }
+    22.5%  { transform: rotate(0deg); }
+    30% { transform: rotate(60deg); }
+    100% { transform: rotate(60deg); }
+}
+
+@media (max-width: 768px) {
+}
+
+@media (min-width: 769px) {
+  .about {
+    text-align: left;
+  }
+
+  .about-div {
+    margin-left: 1rem
+  }
+  h1 {
+    font-size: 3rem;
+    letter-spacing: 1rem;
+  }
+  .logo {
+    height: 700px;
+  }
+
+  .modal-window {
+    max-width: 768px;
+    width: 100%;
+  }
+
+  .modal-content {
+    padding: 1rem 3rem;
+  }
+}
+
+</style>
